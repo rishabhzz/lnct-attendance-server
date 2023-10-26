64e9ae7e83a504e92b4a4eabbb0ad55e9952d557
Rishabh Singh <56092654+rishabhzz@users.noreply.github.com>
2023-10-26 16:40:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ console.log (JSON.stringify(body_param));
 if (body_param.object){
 if (body_param.entry &&
 body_param.entry[0].changes &&
-body_param.entry[0].changes[0].value.message && 
-body_param.entry[0].changes[0].value.message[0]){
+body_param.entry[0].changes[0].value.messages && 
+body_param.entry[0].changes[0].value.messages[0]){
 
   let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
   let from = body_param.entry[0].changes[0].value.messages[0].from;
