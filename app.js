b6934b15b32b4de07475c6cca22369a95f725143
Rishabh Singh <56092654+rishabhzz@users.noreply.github.com>
2023-10-26 18:15:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ body_param.entry[0].changes[0].value.messages[0]){
   let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
   console.log("phone number: " + phon_no_id);
   console.log("message: " + msg_body);
+  console.log("from: " + from);
 
 
 
