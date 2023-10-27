88d5cc7e72a2e24a8c3da8ff863da51f5b302c2d
Rishabh Singh <56092654+rishabhzz@users.noreply.github.com>
2023-10-27 10:11:45 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const body_parser = require('body-parser');
+
+const admin = require('firebase-admin');
+const serviceAccount = require('keys.json');
+
+
 const { subscribe } = require('diagnostics_channel');
 const { default: axios } = require('axios');
 const app = express().use(body_parser.json());
 require('dotenv').config();
 
 const port = process.env.PORT || 3000; // Set the port you want to use
-
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://lnct-attendxnce-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
 
 
 const mytoken = 'rishabh';
@@ -35,7 +43,7 @@ app.get('/webhook', (req, res) => {
 app.post('/webhook', (req, res) => {
 
   let body_param=req.body;
-console.log (JSON.stringify(body_param));
+//console.log (JSON.stringify(body_param));
 
 if (body_param.object){
 if (body_param.entry &&
@@ -51,37 +59,82 @@ body_param.entry[0].changes[0].value.messages[0]){
   console.log("from: " + from);
 
 
+  admin
+    .database()
+    .ref(`users/${from}`)
+    .once('value')
+    .then(snapshot => {
+      const userData = snapshot.val();
 
-  const url = 'https://graph.facebook.com/v17.0/167707166417060/messages';
-  
-  const data = {
-    messaging_product: "whatsapp", 
-  to: from, 
-  text:{
-  body: "Hi.. I'm Rishabh"
-  }
-  };
-  
-  const config = {
-    headers: {
-      Authorization: `Bearer EAASs6XgWjPMBOZB4vjHN1vMn8Gdt0oSWZAaRr5mCvfXmcrJPWa9xOvOIZCZApJm54tUuFMic1lSchMgzbDbokZAfs1K9cg5ZA48rkZCuMZA86gxXyzlmAN3i5BVdwRGTqV77IJECggONeq68fc485uEWZBoxTmr57A63pZC048n0EHScBJ7xCB5J6qKYXvZB38Gmm4c0OtQgfRJgQyiPc2oXAYoVWQW2niLq0MgCYFS`,
-      'Content-Type': 'application/json'
-    }
-  };
-  
-  axios.post(url, data, config)
-    .then(response => {
-      console.log('Response:', success);
+      if (userData) {
+        // User exists in the database
+        const id = userData.id;
+        const pass = userData.pass;
+
+        // Use the childId and childApiToken to make an API call
+        axios
+          .post('https://lnct-attendance-adtm.onrender.com/attendance', {
+            username: id,
+            password: pass,
+            // Other data for the API call
+          })
+          .then(response => {
+            // Handle the API response
+            console.log('API Response:', response.data);
+          })
+          .catch(error => {
+            // Handle API call error
+            console.error('API Error:', error);
+          });
+
+        res.sendStatus(200); // Respond to the webhook request
+      } else {
+        console.log('User not found in the database.');
+        res.sendStatus(200); // Respond to the webhook request
+      }
     })
     .catch(error => {
-      console.error('inside if error');
+      console.error('Firebase database error:', error);
+      res.sendStatus(500); // Respond with an error status
     });
+
+
+
+ 
+
+
+
+
+  // const url = 'https://graph.facebook.com/v17.0/167707166417060/messages';
+  
+  // const data = {
+  //   messaging_product: "whatsapp", 
+  // to: from, 
+  // text:{
+  // body: "Hi.. I'm Rishabh"
+  // }
+  // };
+  
+  // const config = {
+  //   headers: {
+  //     Authorization: `Bearer EAASs6XgWjPMBOZB4vjHN1vMn8Gdt0oSWZAaRr5mCvfXmcrJPWa9xOvOIZCZApJm54tUuFMic1lSchMgzbDbokZAfs1K9cg5ZA48rkZCuMZA86gxXyzlmAN3i5BVdwRGTqV77IJECggONeq68fc485uEWZBoxTmr57A63pZC048n0EHScBJ7xCB5J6qKYXvZB38Gmm4c0OtQgfRJgQyiPc2oXAYoVWQW2niLq0MgCYFS`,
+  //     'Content-Type': 'application/json'
+  //   }
+  // };
+  
+  // axios.post(url, data, config)
+  //   .then(response => {
+  //     console.log('Response:', success);
+  //   })
+  //   .catch(error => {
+      
+  //   });
   
 
   res.sendStatus(200);
 
 }else{
-  console.log('outside');
+  
   res.sendStatus(404);
 }
 }
